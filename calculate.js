6cabbefce5580b185f2bdece64d8c79491a58752
markerta <markert.manda@gmail.com>
2017-08-01 14:10:48 -0400

    Add negation and decimal functionality

--- calculate.js
+++ calculate.js
@@ -1,22 +1,59 @@
 var result = 0;
 var currentNum = 0;
 var operatorClicked = '';
+var operatorClickedLast = false;
 var firstNum = true;
+var isDecimal = false;
+
+function setTotal (text) {
+	if (text.toString().length > 8) {
+		document.getElementById('total_text').firstChild.data = text.toPrecision(4);
+	} else {
+		document.getElementById('total_text').firstChild.data = text;
+	}
+}
+function setTotalFixed (text) {
+	if (text.toString().replace(/^-?\d*\.?|0+$/g, '').length > 3) {
+		if (text.toFixed(2).toString().length > 8) {
+			document.getElementById('total_text').firstChild.data = text.toFixed(2).toPrecision(4);
+		} else {
+			document.getElementById('total_text').firstChild.data = text.toFixed(3);
+		}
+	} else {
+		if (text.toFixed(2).toString().length > 8) {
+			document.getElementById('total_text').firstChild.data = text.toPrecision(4);
+		} else {
+			document.getElementById('total_text').firstChild.data = text;
+		}
+	}
+}
+function setOp (op) {
+	document.getElementById('op_text').firstChild.data = op;
+}
 
 function number (num) {
-	var total = document.getElementById('total_text');
-	var op = document.getElementById('op_text');
+	var numDecimals;
 
-	if (firstNum) op.firstChild.data = '';
-
-	currentNum = currentNum * 10 + num;
-	total.firstChild.data = currentNum;
+	if (operatorClicked === "=") {
+		firstNum = true;
+		result = 0;
+	}
+	if (firstNum) setOp('');
+	if (!isDecimal) {
+		currentNum = currentNum * 10 + num;
+		setTotal(currentNum);
+		
+	} else {
+		numDecimals = currentNum.toString().replace(/^-?\d*\.?|0+$/g, '').length + 1;
+		currentNum = currentNum + num / Math.pow(10, numDecimals);
+		setTotal(currentNum);
+		
+	}
+	operatorClickedLast = false;
 }
 
 function operator (op) {
-	var total = document.getElementById('total_text');
-	var opText = document.getElementById('op_text');
-	if (!firstNum) {
+	if (!firstNum && !(operatorClicked === "=")) {
 		switch (operatorClicked) {
 			case '+': 	result += currentNum;
 						break;
@@ -27,40 +64,62 @@ function operator (op) {
 			case '/': 	result /= currentNum;
 						break;
 		}
-		if (((+result).toFixed(20)).replace(/^-?\d*\.?|0+$/g, '').length > 2)
-			total.firstChild.data = result.toFixed(2);
-		else
-			total.firstChild.data = result;
-	} else {
-		if (operatorClicked != '=') {
-			result = currentNum;
-		}
+		setTotalFixed(result);
+	} else if (firstNum) {
+		result = currentNum;
 	}
 	currentNum = 0;
+	isDecimal = false;
+	operatorClickedLast = true;
 	if (op != '=') {
 		firstNum = false;
 		operatorClicked = op;
-		opText.firstChild.data = op;
+		setOp(op);
 	}
 	else {
-		firstNum = true;
 		operatorClicked = op;
-		opText.firstChild.data = op;
+		setOp(op);
 	}
 }
 
+function decimal () {
+	setTotal(currentNum + ".");
+	isDecimal = true;
+}
+
+function negate() {
+	if (!operatorClickedLast) {
+		if (Math.sign(currentNum) == 1) {
+			currentNum = -Math.abs(currentNum);
+		}
+		else {
+			currentNum = Math.abs(currentNum);
+		}
+		setTotalFixed(currentNum);
+	}
+	else {
+		if (Math.sign(result) == 1) {
+			result = -Math.abs(result);
+		}
+		else {
+			result = Math.abs(result);
+		}
+		setTotalFixed(result);
+	}
+	
+}
+
 function clears (c) {
-	var total = document.getElementById('total_text');
-	var op = document.getElementById('op_text');
-	if (c === "CA" || operatorClicked === "=") {
+	if (c === "CA" || operatorClicked === "=" || firstNum) {
 		result = 0;
 		firstNum = true;
 		operatorClicked = '';
-		total.firstChild.data = 0;
-		op.firstChild.data = '';
+		setTotal(0);
+		setOp('');
 	} else {
-		total.firstChild.data = '_';
+		setTotal('_');
 	}
+	isDecimal = false;
 	currentNum = 0;
 }
 
@@ -80,6 +139,9 @@ var subtract = document.getElementById('subtract');
 var multiply = document.getElementById('multiply');
 var divide = document.getElementById('divide');
 var equals = document.getElementById('equals');
+
+var dot = document.getElementById('dot');
+var neg = document.getElementById('neg');
 
 var clear = document.getElementById('clear');
 var clearAll = document.getElementById('clear_all')
@@ -129,6 +191,14 @@ divide.onclick = function() {
 }
 equals.onclick = function() {
 	operator('=');
+}
+
+dot.onclick = function() {
+	decimal();
+}
+
+neg.onclick = function() {
+	negate();
 }
 
 clear.onclick = function() {
